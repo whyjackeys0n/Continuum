dc26f3126cc7499d192297a1daa76741f7a939a6
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-07-23 20:02:09 -0400

    con

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
 import pybamm
-import numpy as np
-import os
-import matplotlib.pyplot as plt
 
-os.chdir(pybamm.__path__[0] + '/..')
+# Choose the Model
+model = pybamm.lithium_ion.DFN()
 
-model = pybamm.lithium_ion.SPM()
-variable = list(model.rhs.keys())[1]
-equation = list(model.rhs.values())[1]
-print('rhs equation for variable \'', variable, '\' is:')
-path = 'examples/notebooks/models/'
-equation.visualise(path + 'spm1.png')
-
+sim = pybamm.Simulation(model)
+sim.solve(t_eval=[0, 3600])
+sim.plot()
